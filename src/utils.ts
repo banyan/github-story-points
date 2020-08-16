4f3e1012558c95f77cb02474b8da1172d706059e
Kohei Hasegawa <ameutau@gmail.com>
2020-08-16 12:15:13 +0900

    Run yarn fmt

--- src/utils.ts
+++ src/utils.ts
@@ -3,11 +3,11 @@ export const debounce = <F extends Function>(func: F, waitMs: number): F => {
   let timeoutID: number;
 
   // conversion through any necessary as it wont satisfy criteria otherwise
-  return <F>(<any>function(this: any, ...args: any[]) {
+  return <F>(<any>function (this: any, ...args: any[]) {
     clearTimeout(timeoutID);
     const context = this;
 
-    timeoutID = window.setTimeout(function() {
+    timeoutID = window.setTimeout(function () {
       func.apply(context, args);
     }, waitMs);
   });
